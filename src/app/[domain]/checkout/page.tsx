5f1acb7260aaa3a903ff84b1c9f33be6b03af214
jedpattersonpaddle <jed.patterson@paddle.com>
2025-04-18 17:03:15 +0100

    add dynamic metadata

--- src/app/[domain]/checkout/page.tsx
+++ src/app/[domain]/checkout/page.tsx
@@ -5,6 +5,25 @@ import {
 } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import CheckoutClient from "./CheckoutClient";
+import type { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ domain: string }>;
+}): Promise<Metadata> {
+  const domain = decodeURIComponent((await params).domain);
+  const showcase = await db
+    .select()
+    .from(ShowcaseSchema)
+    .where(eq(ShowcaseSchema.subdomain, domain))
+    .limit(1);
+
+  return {
+    title: `${showcase[0].companyName} - Checkout`,
+    description: `Checkout page for ${showcase[0].companyName}`,
+  };
+}
 
 async function getShowcase(domain: string) {
   const showcase = await db
